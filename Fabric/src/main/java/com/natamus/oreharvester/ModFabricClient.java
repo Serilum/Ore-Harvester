841fb143e44c8b88b3134876304f8c625178d7ce
Rick South <rick@natamus.com>
2024-07-28 07:22:34 +0200

    (v1.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/oreharvester/ModFabricClient.java
+++ Fabric/src/main/java/com/natamus/oreharvester/ModFabricClient.java
@@ -3,11 +3,17 @@ package com.natamus.oreharvester;
 import com.natamus.collective.fabric.callbacks.CollectiveClientEvents;
 import com.natamus.oreharvester.events.WorldEvents;
 import net.fabricmc.api.ClientModInitializer;
+import com.natamus.oreharvester.util.Reference;
+import com.natamus.collective.check.ShouldLoadCheck;
 import net.minecraft.client.multiplayer.ClientLevel;
 
 public class ModFabricClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() { 
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		registerEvents();
 	}
 	
--- Forge/src/main/java/com/natamus/oreharvester/ModForge.java
+++ Forge/src/main/java/com/natamus/oreharvester/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.oreharvester;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.oreharvester.forge.config.IntegrateForgeConfig;
 import com.natamus.oreharvester.forge.events.ForgeOreMineEvents;
 import com.natamus.oreharvester.forge.events.ForgeWorldEvents;
@@ -16,6 +17,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
