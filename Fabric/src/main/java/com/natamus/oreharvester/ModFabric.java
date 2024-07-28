e681f547d12b9d7b25bae5b6658440007d4dd30f
Rick South <rick@natamus.com>
2024-07-28 07:22:27 +0200

    (v1.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/oreharvester/ModFabric.java
+++ Fabric/src/main/java/com/natamus/oreharvester/ModFabric.java
@@ -1,6 +1,7 @@
 package com.natamus.oreharvester;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.collective.fabric.callbacks.CollectivePlayerEvents;
 import com.natamus.oreharvester.events.OreMineEvents;
 import com.natamus.oreharvester.events.WorldEvents;
@@ -20,6 +21,10 @@ public class ModFabric implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
